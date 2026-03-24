327c7090af79bc07458c2dd41844057faf55ffb9
Mehul Arora <mehul@s2.dev>
2026-03-24 12:57:41 +0530

    .

--- common/src/types/config.rs
+++ common/src/types/config.rs
@@ -36,8 +36,10 @@ use crate::maybe::Maybe;
 /// Encryption algorithm.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, strum::Display, strum::EnumString)]
 pub enum EncryptionAlgorithm {
+    /// AEGIS-256
     #[strum(serialize = "aegis-256")]
     Aegis256,
+    /// AES-256-GCM
     #[strum(serialize = "aes-256-gcm")]
     Aes256Gcm,
 }
