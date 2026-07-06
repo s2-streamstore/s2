4c416ecc6b300f2acfaf3f7f7111680a7350d5c7
Shikhar Bhushan <shikhar@s2.dev>
2026-07-06 14:09:38 -0700

    Revert "chore: remove unused From<BasinName> for BasinNamePrefix impl" (#625)
    Reverts s2-streamstore/s2#622

--- common/src/basin.rs
+++ common/src/basin.rs
@@ -173,6 +173,12 @@ impl Default for BasinNamePrefix {
     }
 }
 
+impl From<BasinName> for BasinNamePrefix {
+    fn from(value: BasinName) -> Self {
+        Self(value.0, PhantomData)
+    }
+}
+
 pub type BasinNameStartAfter = BasinNameStr<StartAfterProps>;
 
 impl Default for BasinNameStartAfter {
